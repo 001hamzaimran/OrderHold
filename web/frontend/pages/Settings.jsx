01612f53fdd0af8aa972363787a50a03b3c2b47b
001hamzaimran <syed.hamza2004@gmail.com>
2025-12-10 14:38:08 +0500

    Change OrderAPI

--- web/frontend/pages/Settings.jsx
+++ web/frontend/pages/Settings.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from 'react-redux';
-import "./Settings.css";
+import "./settings.css";
 
 function Settings() {
   const Shop = useSelector((state) => state.store.storeDetail);
